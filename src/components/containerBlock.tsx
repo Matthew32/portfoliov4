c2b0d0673d47b27a09244b38bc70564f15340f20
Matthew <mmartinezmateo93@gmail.com>
2023-05-18 21:47:53 +0200

    change meta info

--- src/components/containerBlock.tsx
+++ src/components/containerBlock.tsx
@@ -39,7 +39,7 @@ export default function ContainerBlock({
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mannupaaji" />
+        <meta name="twitter:site" content="@Matt92519047" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
